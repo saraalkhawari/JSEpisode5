65b83c3c8d16df8b594233a4c03cd8dd46ae8778
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-10-31 20:15:13 +0300

    Fay and Sara First Commit

--- bareed.js
+++ bareed.js
@@ -43,11 +43,13 @@ class Point {
  **********************************************************/
 class Wallet {
   // implement Wallet!
-  constructor(money = 0) {}
+  constructor(money = 0) {
+    this.money = money;
+  }
 
-  credit = amount => {};
+  credit = amount => (this.money += amount);
 
-  debit = amount => {};
+  debit = amount => (this.money -= amount);
 }
 
 /**********************************************************
@@ -62,7 +64,15 @@ class Wallet {
  * let person = new Person(name, x, y);
  **********************************************************/
 class Person {
-  // implement Person!
+  constructor(name, x, y) {
+    this.name = name;
+    this.location = new Point(x, y);
+  }
+  wallet = new Wallet();
+
+  moveTo = newPoint => {
+    this.location = newPoint;
+  };
 }
 
 /**********************************************************
@@ -80,8 +90,15 @@ class Person {
  *
  * new vendor = new Vendor(name, x, y);
  **********************************************************/
-class Vendor {
-  // implement Vendor!
+class Vendor extends Person {
+  range = 5;
+  price = 1;
+
+  sellTo = (customer, numberOfIceCreams) => {
+    this.moveTo(customer.location);
+    this.wallet.credit(numberOfIceCreams * this.price);
+    customer.wallet.debit(numberOfIceCreams * this.price);
+  };
 }
 
 /**********************************************************
@@ -100,8 +117,25 @@ class Vendor {
  *
  * new customer = new Customer(name, x, y);
  **********************************************************/
-class Customer {
-  // implement Customer!
+class Customer extends Person {
+  wallet = new Wallet(10);
+
+  _isInRange = vendor => {
+    return this.location.distanceTo(vendor.location) <= vendor.range;
+  };
+
+  _haveEnoughMoney = (vendor, numberOfIceCreams) => {
+    return this.wallet.money >= numberOfIceCreams * vendor.price;
+  };
+
+  requestIceCream = (vendor, numberOfIceCreams) => {
+    return this._isInRange(vendor) &&
+      this._haveEnoughMoney(vendor, numberOfIceCreams)
+      ? vendor.sellTo(this, numberOfIceCreams)
+      : console.log("You Do not deserve Bareed");
+    //   return ;
+    // else {return console.log('You Do not deserve Bareed')};
+  };
 }
 
 export { Point, Wallet, Person, Customer, Vendor };
